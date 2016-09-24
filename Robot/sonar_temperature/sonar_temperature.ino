7ead40e494190a8f4ef32b48848df8b2c9741d22
jh10422 <jjh10422@gmail.com>
2016-09-24 12:39:38 -0700

    V 1.8

--- Robot/sonar_temperature/sonar_temperature.ino
+++ Robot/sonar_temperature/sonar_temperature.ino
@@ -1,14 +1,15 @@
 /**
  * @author tedfoodlin
  * @author Jiajer Ho (jh10422)
+ * @author "Neil" (5caryT001sMan)
  * 
- * Underwater Sonar Sensor v1.2
+ * v1.8
 */
 
 #include <Wire.h>
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
-#include <NewPing.h>
+
 #include <Thermistor.h>
 #include <Servo.h>
 Thermistor temp(0);
@@ -22,35 +23,69 @@ Thermistor temp(0);
 #define D6_pin  6
 #define D7_pin  7
 int sensorPin = 1;
+float vPow = 4.7;
+float r1 = 100000;
+float r2 = 10000;
+int piezoPin = 7;
+int button1 = 4; //button pin, connect to ground to activate buzzer
 
 int buttonpin1 = 10; // button pin for alarm
 int press1 = 0;
-int buzzer = A5; // pin for buzzer  alarm
-
+int alarm = 0;
+int mute = 0;
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define MAX_DISTANCE 500 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 #define SERIESRESISTOR 10000    
 #define THERMISTORPIN A0 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);// NewPing setup of pins and maximum distance.
+
 
 LiquidCrystal_I2C lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
 
 void setup()
 {
+   pinMode(button1, INPUT);
+   digitalWrite(button1, HIGH); //enable pullups to make pin high
+   Serial.begin(9600);
+   
+   // Send ANSI terminal codes
+   Serial.print("\x1B");
+   Serial.print("[2J");
+   Serial.print("\x1B");
+   Serial.println("[H");
+   // End ANSI terminal codes
+   
+   Serial.println("--------------------");
+   Serial.println("DC VOLTMETER");
+   Serial.print("Maximum Voltage: ");
+   Serial.print((int)(vPow / (r2 / (r1 + r2))));
+   Serial.println("V");
+   Serial.println("--------------------");
+   Serial.println("");
+   
+
   pinMode (buttonpin1, INPUT);
   // initialize serial communication
-  Serial.begin(9600);
   
-  lcd.begin (12,2);
+  lcd.begin (16,2);
  
   // LCD Backlight ON
   lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
   lcd.setBacklight(HIGH);
 
   lcd.home (); // go home on LCD
+ lcd.setBacklight(HIGH);
 
-  delay(1500);
+  lcd.home (); // go home on LCD
+  lcd.setCursor(0,0);
+  lcd.print("45C ROBOTICS  ");
+  lcd.setCursor(0,1);
+  lcd.print("VERSION: 1.8  ");  
+    delay(1500);
+    
+  lcd.begin (16,2);
+
+
   Serial.println(" ");
   Serial.println("Sonar and temperature sensors enabled!");
 
@@ -59,7 +94,11 @@ void setup()
 
 void loop()
 {
+   press1 = digitalRead(button1);
 
+  //tone(piezoPin, 1000, 500);
+  //delay(1000);
+  
   int reading = analogRead(sensorPin);  
   // converting that reading to voltage, for 3.3v arduino use 3.3
   float voltage = reading * 5.0;
@@ -72,90 +111,76 @@ void loop()
   float temperatureC = (voltage - 0.5) * 100; // converting from 10 mv per degree wit 500 mV offset to degrees ((voltage - 500mV) times 100)
   
   int temperature = temp.getTemp();
-  double uS = sonar.ping() * 1.0951047207; // Send ping, get calibrated ping time in microseconds (uS).
-  unsigned int cm = ((1404.3 + 4.7*temperature - (0.04 * pow(temperature, 2))) * 0.00005114827 * uS) + 5; //gg magic calculations
 
-  String altitudeDisplay = "";
-  String altitudeDisplayUnits = " cm";
-  String finalAltitudeDisplay = altitudeDisplay + cm + altitudeDisplayUnits;
-  String temperatureDisplay = "";
+
+  String altitudeDisplay = "ALT:";
+  String altitudeDisplayUnits = "";
+
+  String temperatureDisplay = "TEMP:";
   String temperatureDisplayUnits = "C";
   String finalTemperatureDisplay = temperatureDisplay + temperature + temperatureDisplayUnits;
   String tempOfControlBox = "Temperature of Control Box: ";
   String finalControlBoxTempDisplay = tempOfControlBox + temperatureC + temperatureDisplayUnits;
+lcd.setCursor (0,0); // go to start of 2nd line
+lcd.print("ALT:102");
 
-  lcd.setCursor (0,0); // go to start of 2nd line
-    lcd.print(finalAltitudeDisplay);
-      delay(1000);
-  lcd.setCursor (5,0); // go to start of 2nd line
-    lcd.print(finalTemperatureDisplay);
+    lcd.setCursor(9,0); // go to start of 2nd line
 
-  lcd.setCursor (5,1);
-    lcd.print(temperatureC);
-      delay(500);
+      lcd.print(finalTemperatureDisplay);
+          delay(500);
+  
+
+      
+        float v = (analogRead(1) * vPow) / 1024.0;
+   float v2 = v / (r2 / (r1 + r2));
+   // Send ANSI terminal codes
+   Serial.print("\x1B");
+   Serial.print("[1A");
+   // End ANSI terminal codes
  
-  float temperatureC = (voltage - 0.5) * 100;  
-  lcd.setCursor (0,1);
-    lcd.print(temperatureC);
-        
-  Serial.println(finalAltitudeDisplay);
+   float v3 = v2+1;
+    lcd.setCursor (0,1);
+          lcd.print("");
+ if (v3 < 11.8){
+   lcd.setCursor(0,1);
+          lcd.print("01 LOW VOLTAGE        ");
+if (mute < 1){
+           tone(piezoPin, 2500);
+  delay(500);
+   noTone(piezoPin);
+
+alarm = alarm + 1;
+}
+}
+if (press1 == HIGH){
+  if (alarm > 1){
+  mute = mute + 1;
+       lcd.setCursor(0,1);
+ lcd.print("02 ALARM MUTED       ");
+  }
+  else
+ {
+   lcd.print("02 ALARM TEST        ");
+           tone(piezoPin, 2500);
+  delay(500);
+   noTone(piezoPin);
+ }
+}
+  else if (v3 > 11.8){
+
+  lcd.setCursor(0,1);
+ lcd.print("SYSTEM HEALTHY       ");
+
   Serial.println(finalTemperatureDisplay);
   Serial.println(finalControlBoxTempDisplay);
   // reportState();
 
   press1 = digitalRead(buttonpin1);
-  if (press1 == HIGH)
-  {
-    lcd.begin (16,2);
-    lcd.print("***ALARM TEST***");
-    lcd.setCursor (0,1);
-    lcd.print("");
-    tone(buzzer, 1000); // Send 1KHz sound signal...
-      delay(1000);        // ...for 1 sec
-    noTone(buzzer);     // Stop sound...
-      delay(1000); 
-  }
-  delay(1000);
-
-}
-
-// testing
-int numberOfTests = 0;
-void reportState()
-{
-  numberOfTests = numberOfTests + 1;
-  String testNumberDisplay = "Test Number: ";
-  String finalTestNumberDisplay = testNumberDisplay + numberOfTests;
-  Serial.println(finalTestNumberDisplay);
-  Serial.println(" ");
-
-  double expectedUS = sonar.ping();
-  unsigned int expectedCM = sonar.convert_cm(expectedUS);
-  double uS = sonar.ping() * 1.0951047207; // Send ping, get calibrated ping time in microseconds (uS).
-
-  String pingTimeDisplay = "Ping Time: ";
-  String pingTimeDisplayUnits = " uS";
-  String finalPingTimeDisplay = pingTimeDisplay + uS + pingTimeDisplayUnits;
-
-  String expAltitudeDisplay = "Expected Altitude: ";
-  String expPingTimeDisplay = "Expected Ping Time: ";
-  String altitudeDisplayUnits = " cm";
-  String expFinalAltitudeDisplay = expAltitudeDisplay + expectedCM + altitudeDisplayUnits;
-  String expFinalPingTimeDisplay = expPingTimeDisplay + expectedUS + pingTimeDisplayUnits;
-
-  double pingTimeFactor = expectedUS / uS;
-  String pingTimeFactorDisplay = "Ping Time Factor: ";
-  String finalPingTimeFactorDisplay = pingTimeFactorDisplay + pingTimeFactor;
-
-  Serial.println(finalPingTimeDisplay);
-  Serial.println(" ");
-  Serial.println(expFinalAltitudeDisplay);
-
-  Serial.println(expFinalPingTimeDisplay);
-  Serial.println(" ");
-  Serial.println(finalPingTimeFactorDisplay);
-  Serial.println(" ");
   
-  delay(10);
+  }
+
+
 }
+
+
 
