d73dd05644166c1213bba464e752f46620519b2a
jh10422 <jjh10422@gmail.com>
2016-10-23 14:54:23 -0700

    update servo

--- Robot/servo/servo.ino
+++ Robot/servo/servo.ino
@@ -9,11 +9,15 @@
 int button1 = 4; //button pin, connect to ground to move servo
 int press1 = 0;
 Servo servo1;
+Servo myservo;  // create servo object to control a servo
 
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
 void setup()
 {
  pinMode(button1, INPUT);
  servo1.attach(7);
+   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
  digitalWrite(4, HIGH); //enable pullups to make pin high
 }
 
@@ -27,5 +31,9 @@ void loop()
  else {
    servo1.write(0);
  }
+   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  myservo.write(val);                  // sets the servo position according to the scaled value
+
 }
 
